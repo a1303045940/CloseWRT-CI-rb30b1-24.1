8fdb2848c6c61b098f1d251eeaa8d8e492211856
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-09-25 16:23:25 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -54,7 +54,7 @@ UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
 UPDATE_PACKAGE "passwall2" "xiaorouji/openwrt-passwall2" "main" "pkg"
 
-UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
+#UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 
 UPDATE_PACKAGE "luci-app-store" "kenzok8/small-package" "main" "pkg"
 UPDATE_PACKAGE "quickstart" "kenzok8/small-package" "main" "pkg"
