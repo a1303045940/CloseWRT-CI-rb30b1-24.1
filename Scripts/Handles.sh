e24a8a3c7f10a99089cadacbac7b13c70bc814e2
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-09-26 10:03:49 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -99,15 +99,6 @@ if [ -d *"openclash"* ]; then
 	cd $PKG_PATCH && echo "openclash date has been updated!"
 fi
 
-#移除Shadowsocks组件
-PW_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-passwall/Makefile")
-if [ -f "$PW_FILE" ]; then
-	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_Shadowsocks_Libev/,/x86_64/d' $PW_FILE
-	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_ShadowsocksR/,/default n/d' $PW_FILE
-	sed -i '/Shadowsocks_NONE/d; /Shadowsocks_Libev/d; /ShadowsocksR/d' $PW_FILE
-
-	cd $PKG_PATCH && echo "passwall has been fixed!"
-fi
 
 SP_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-ssr-plus/Makefile")
 if [ -f "$SP_FILE" ]; then
