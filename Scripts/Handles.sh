2f53a534cf9942b1d2bb3580fc54642f9cd48b93
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-09-28 18:37:09 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -67,6 +67,26 @@ if [[ -f "${RUST_MAKEFILE}" ]]; then
 else
   echo "File ${RUST_MAKEFILE} does not exist." >&2
 fi
+
+#移除Shadowsocks组件
+PW_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-passwall/Makefile")
+if [ -f "$PW_FILE" ]; then
+	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_Shadowsocks_Libev/,/x86_64/d' $PW_FILE
+	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_ShadowsocksR/,/default n/d' $PW_FILE
+	sed -i '/Shadowsocks_NONE/d; /Shadowsocks_Libev/d; /ShadowsocksR/d' $PW_FILE
+
+	cd $PKG_PATH && echo "passwall has been fixed!"
+fi
+
+SP_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-ssr-plus/Makefile")
+if [ -f "$SP_FILE" ]; then
+	sed -i '/default PACKAGE_$(PKG_NAME)_INCLUDE_Shadowsocks_Libev/,/libev/d' $SP_FILE
+	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_ShadowsocksR/,/x86_64/d' $SP_FILE
+	sed -i '/Shadowsocks_NONE/d; /Shadowsocks_Libev/d; /ShadowsocksR/d' $SP_FILE
+
+	cd $PKG_PATH && echo "ssr-plus has been fixed!"
+fi
+
 #预置OpenClash内核和数据
 if [ -d *"openclash"* ]; then
         echo "预置OpenClash内核和数据!"
