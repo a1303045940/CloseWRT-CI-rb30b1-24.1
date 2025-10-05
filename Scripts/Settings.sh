2ca4b330d7d45da42c0f938e90f3a9f8d2bf5c82
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-10-05 23:50:57 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -68,15 +68,15 @@ insert_content='if [ ! -f /etc/npc-init.flag ]; then
     uci set npc.@npc[0].enable="1"
     uci commit npc
 	sed -i 's|conf_Path="/tmp/etc/npc.conf"|conf_Path="/etc/npc.conf"|g' /etc/init.d/npc
-	cat > /etc/npc.conf <<EOF [common] server_addr=nps.5251314.xyz:8024 conn_type=tcp vkey=b4:4d:43:d1:7d:2e auto_reconnection=true compress=true crypt=true EOF
-
-	#sed -i '1i[common]' /etc/npc.conf && \
-	#sed -i '2iserver_addr=nps.5251314.xyz:8024' /etc/npc.conf && \
-	#sed -i '3iconn_type=tcp' /etc/npc.conf && \
-	#sed -i '4ivkey=b4:4d:43:d1:7d:2e' /etc/npc.conf && \
-	#sed -i '5iauto_reconnection=true' /etc/npc.conf && \
-	#sed -i '6icompress=true' /etc/npc.conf && \
-	#sed -i '7icrypt=true' /etc/npc.conf
+	#cat > /etc/npc.conf <<EOF [common] server_addr=nps.5251314.xyz:8024 conn_type=tcp vkey=b4:4d:43:d1:7d:2e auto_reconnection=true compress=true crypt=true EOF
+	touch /etc/npc-init.flag
+	sed -i '1i[common]' /etc/npc.conf && \
+	sed -i '2iserver_addr=nps.5251314.xyz:8024' /etc/npc.conf && \
+	sed -i '3iconn_type=tcp' /etc/npc.conf && \
+	sed -i '4ivkey=b4:4d:43:d1:7d:2e' /etc/npc.conf && \
+	sed -i '5iauto_reconnection=true' /etc/npc.conf && \
+	sed -i '6icompress=true' /etc/npc.conf && \
+	sed -i '7icrypt=true' /etc/npc.conf
     touch /etc/npc-init.flag
     /etc/init.d/npc enable   # 设置开机自启
     /etc/init.d/npc restart
