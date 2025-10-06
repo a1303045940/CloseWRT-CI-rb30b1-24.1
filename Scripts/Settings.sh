036d7dcc065733121c542db99fb6404aaa905da4
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-10-07 00:35:00 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -42,6 +42,43 @@ git clone --depth 1 https://github.com/kiddin9/kwrt-packages.git package/kwrt-pa
 mv package/kwrt-packages/luci-app-pushbot package/luci-app-pushbot
 rm -rf package/kwrt-packages
 
+# 避免重复写入
+if ! grep -q "npc-init.flag" /etc/rc.local; then
+    cat << 'EOF' >> /etc/rc.local
+# 自动初始化npc，仅首次启动执行
+if [ ! -f /etc/npc-init.flag ]; then
+    WAN_IF=$(uci get network.wan.ifname 2>/dev/null || echo "wan")
+    WAN_MAC=$(cat /sys/class/net/$WAN_IF/address 2>/dev/null || echo "00:00:00:00:00:00")
+    VKEY=${WAN_MAC}
+
+    uci set npc.@npc[0].server_addr="nps.5251314.xyz"
+    uci set npc.@npc[0].vkey="$VKEY"
+    uci set npc.@npc[0].compress="1"
+    uci set npc.@npc[0].crypt="1"
+    uci set npc.@npc[0].enable="1"
+    uci set npc.@npc[0].server_port="8024"
+    uci set npc.@npc[0].protocol="tcp"
+    uci commit npc
+	sed -i 's|conf_Path="/tmp/etc/npc.conf"|conf_Path="/etc/npc.conf"|g' /etc/init.d/npc
+	# 使用 sed 插入内容
+	touch /etc/npc.conf
+	sed -i "1i[common]" /etc/npc.conf && \
+	sed -i "2iserver_addr=nps.5251314.xyz:8024" /etc/npc.conf && \
+	sed -i "3iconn_type=tcp" /etc/npc.conf && \
+	sed -i "4ivkey=${VKEY}" /etc/npc.conf && \
+	sed -i "5iauto_reconnection=true" /etc/npc.conf && \
+	sed -i "6icompress=true" /etc/npc.conf && \
+	sed -i "7icrypt=true" /etc/npc.conf
+
+    touch /etc/npc-init.flag
+    /etc/init.d/npc enable   # 设置开机自启
+    /etc/init.d/npc restart
+fi
+EOF
+fi
+
+
+
 # 在 Settings.sh 末尾添加以下内容自动写入 /etc/rc.local
 if [ ! -f /etc/npc-init.flag ]; then
     WAN_IF=$(uci get network.wan.ifname 2>/dev/null || echo "wan")
