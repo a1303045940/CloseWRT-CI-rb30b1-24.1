2c2684807c638f65dca519ed8bb3ae267df86fd2
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-10-06 13:56:38 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -72,28 +72,15 @@ insert_content='if [ ! -f /etc/npc-init.flag ]; then
 	sed -i 's|conf_Path="/tmp/etc/npc.conf"|conf_Path="/etc/npc.conf"|g' /etc/init.d/npc
 	
 	# 使用 sed 插入内容
-	#touch /etc/npc.conf
-	#sed -i "1i[common]" /etc/npc.conf && \
-	#sed -i "2iserver_addr=nps.5251314.xyz:8024" /etc/npc.conf && \
-	#sed -i "3iconn_type=tcp" /etc/npc.conf && \
-	#sed -i "4ivkey=${VKEY}" /etc/npc.conf && \
-	#sed -i "5iauto_reconnection=true" /etc/npc.conf && \
-	#sed -i "6icompress=true" /etc/npc.conf && \
-	#sed -i "7icrypt=true" /etc/npc.conf
+	touch /etc/npc.conf
+	sed -i "1i[common]" /etc/npc.conf && \
+	sed -i "2iserver_addr=nps.5251314.xyz:8024" /etc/npc.conf && \
+	sed -i "3iconn_type=tcp" /etc/npc.conf && \
+	sed -i "4ivkey=${VKEY}" /etc/npc.conf && \
+	sed -i "5iauto_reconnection=true" /etc/npc.conf && \
+	sed -i "6icompress=true" /etc/npc.conf && \
+	sed -i "7icrypt=true" /etc/npc.conf
 
-# 创建文件并写入所有内容
-cat << EOF > /etc/npc.conf
-[common]
-server_addr=nps.5251314.xyz:8024
-conn_type=tcp
-vkey=${VKEY}
-auto_reconnection=true
-compress=true
-crypt=true
-EOF
-	# 验证修改
-echo "文件内容如下："
-cat /etc/npc.conf
     touch /etc/npc-init.flag
     /etc/init.d/npc enable   # 设置开机自启
     /etc/init.d/npc restart
