e5394019af3eea4a577235f1c604d8876ea33af3
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-10-07 10:16:30 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -42,52 +42,11 @@ git clone --depth 1 https://github.com/kiddin9/kwrt-packages.git package/kwrt-pa
 mv package/kwrt-packages/luci-app-pushbot package/luci-app-pushbot
 rm -rf package/kwrt-packages
 
+
+#开机启动文件rc.local替换
 cp "$GITHUB_WORKSPACE/Scripts/npc/rc.local" "$GITHUB_WORKSPACE/package/base-files/files/etc/rc.local"
 chmod +x "$GITHUB_WORKSPACE/package/base-files/files/etc/rc.local"
 
-# 在 Settings.sh 末尾添加以下内容自动写入 /etc/rc.local
-RCLOCAL="package/base-files/files/etc/rc.local"
-TMPFILE="package/base-files/files/etc/.rc.local_insert"
-
-# 只有没有插入过才插入（通过唯一标识判断）
-if ! grep -q 'npc-init.flag' "$RCLOCAL"; then
-cat << 'EOF' > "$TMPFILE"
-if [ ! -f /etc/npc-init.flag ]; then
-    WAN_IF=$(uci get network.wan.ifname 2>/dev/null || echo "wan")
-    WAN_MAC=$(cat /sys/class/net/$WAN_IF/address)
-    #VKEY=$(echo -n "$WAN_MAC" | md5sum | awk '{print $1}')
-    VKEY=${WAN_MAC}
-
-    uci set npc.@npc[0].server_addr="nps.5251314.xyz"
-    uci set npc.@npc[0].vkey="$VKEY"
-    uci set npc.@npc[0].compress="1"
-    uci set npc.@npc[0].crypt="1"
-    uci set npc.@npc[0].enable="1"
-    uci set npc.@npc[0].server_port="8024"
-    uci set npc.@npc[0].protocol="tcp"
-    uci commit npc
-    sed -i 's|conf_Path="/tmp/etc/npc.conf"|conf_Path="/etc/npc.conf"|g' /etc/init.d/npc
-    # 使用 sed 插入内容
-    touch /etc/npc.conf
-    sed -i "1i[common]" /etc/npc.conf && \
-    sed -i "2iserver_addr=nps.5251314.xyz:8024" /etc/npc.conf && \
-    sed -i "3iconn_type=tcp" /etc/npc.conf && \
-    sed -i "4ivkey=${VKEY}" /etc/npc.conf && \
-    sed -i "5iauto_reconnection=true" /etc/npc.conf && \
-    sed -i "6icompress=true" /etc/npc.conf && \
-    sed -i "7icrypt=true" /etc/npc.conf
-
-    /etc/init.d/npc enable   # 设置开机自启
-    /etc/init.d/npc restart
-    touch /etc/npc-init.flag
-fi
-EOF
-
-    # 在exit 0前插入
-    sed -i "/^exit 0$/r $TMPFILE" "$RCLOCAL"
-    chmod +x "$RCLOCAL"
-    rm -f "$TMPFILE"
-fi
 
 #调整mtk系列配置
 sed -i '/TARGET.*mediatek/d' ./.config
