de619b5499b819a577b0e323d0c7c0b663a29650
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-10-20 23:41:56 +0800

    Update filogic.mk

--- r30b1/filogic.mk
+++ r30b1/filogic.mk
@@ -351,8 +351,8 @@ endef
 TARGET_DEVICES += cmcc_rax3000m-nand
 
 define Device/clt-r30b1-112M
-  DEVICE_VENDOR := clt
-  DEVICE_MODEL := CLT R30B1
+  DEVICE_VENDOR := CLT
+  DEVICE_MODEL := R30B1
   DEVICE_DTS := mt7981b-clt-r30b1-112M
   DEVICE_DTS_DIR := ../dts
   DEVICE_PACKAGES := kmod-mt7981-firmware mt7981-wo-firmware
