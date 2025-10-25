d9a02443af2da7790b0a9e346e1150b996d78b79
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-10-25 23:19:42 +0800

    Update filogic.mk

--- r30b1/filogic.mk
+++ r30b1/filogic.mk
@@ -359,7 +359,7 @@ define Device/clt_r30b1-112m
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
   PAGESIZE := 2048
-  IMAGE_SIZE := 116736k
+  IMAGE_SIZE := 114688k
   KERNEL_IN_UBI := 1
   IMAGES += factory.bin
   IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
