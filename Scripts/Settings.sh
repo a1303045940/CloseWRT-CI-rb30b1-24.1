75e9c7f43b49d669b9a3b566029139fa74cae1e7
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-10-26 01:31:25 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -35,6 +35,15 @@ if [[ $WRT_URL != *"lede"* ]]; then
 	
 	# 添加两行代码到 exit 0 前面
 	sed -i '/^exit 0$/i sed -i "s,7981/packages,filogic/packages,g" /etc/opkg/distfeeds.conf' package/emortal/default-settings/files/99-default-settings-chinese
+	#修改默认WIFI名
+	sed -i "s/\.ssid=.*/\.ssid=OpenWrt/g" $(find ./package/kernel/mac80211/ ./package/network/config/ -type f -name "mac80211.*")
+	
+	
+	# 修改 2.4GHz Radio 的默认SSID
+	#sed -i '/band="2g"/,/${.*ssid=.*/s/\.ssid=.*/\.ssid=Your-OpenWrt-2.4G/}' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+	
+	# 修改 5GHz Radio 的默认SSID
+	#sed -i '/band="5g"/,/${.*ssid=.*/s/\.ssid=.*/\.ssid=Your-OpenWrt-5G/}' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 fi
 
