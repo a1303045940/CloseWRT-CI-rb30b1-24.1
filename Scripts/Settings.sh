fad1723508db9bfc8116672591560e7c15e7e678
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-10-26 21:06:39 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -92,10 +92,10 @@ if [[ $WRT_URL == *"lede"* ]]; then
 
 	
 	# 修改 2.4GHz Radio 的默认SSID
-	#sed -i '/band="2g"/,/${.*ssid=.*/s/\.ssid=.*/\.ssid=Your-OpenWrt-2.4G/}' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+	sed -i '/band="2g"/,/${.*ssid=.*/s/\.ssid=.*/\.ssid=Your-OpenWrt-2.4G/}' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
 	
 	# 修改 5GHz Radio 的默认SSID
-	#sed -i '/band="5g"/,/${.*ssid=.*/s/\.ssid=.*/\.ssid=Your-OpenWrt-5G/}' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+	sed -i '/band="5g"/,/${.*ssid=.*/s/\.ssid=.*/\.ssid=Your-OpenWrt-5G/}' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 fi
 
