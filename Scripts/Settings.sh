8fc3f7bb667b97359e66770bf2a619d59e4eb2d0
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-10-26 14:54:26 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -79,7 +79,7 @@ if [[ $WRT_URL == *"lede"* ]]; then
 	# 修改版本为编译日期
 	date_version=$(date +"%y.%m.%d")
 	orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-	sed -i "s/${orig_version}/R${date_version} by vx:Mr___zjz/g" package/lean/default-settings/files/zzz-default-settings
+	sed -i "s/${orig_version}/R${date_version} by vx:Mr___zjz  /g" package/lean/default-settings/files/zzz-default-settings
 	
 	#修改luc显示版本改成系统版本
 	sed -i "735s/<%=pcdata(ver\.luciname)%> (<%=pcdata(ver\.luciversion)%>)/openwrt-24.10.3/" package/lean/autocore/files/arm/index.htm
