7371fbbe4b10bf8c34f6d35594875a72a0f060f3
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-10-27 14:20:17 +0800

    Update Settings-lede.sh

--- Scripts/Settings-lede.sh
+++ Scripts/Settings-lede.sh
@@ -64,13 +64,6 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 	#修改默认WIFI名
 	sed -i "s/\.ssid=.*/\.ssid=OpenWrt/g" $(find ./package/kernel/mac80211/ ./package/network/config/ -type f -name "mac80211.*")
 
-
-	# 替换 2.4G 的 .ssid
-	sed -i '/band="2g"/, /.*ssid=.*/ s/\.ssid=.*/.ssid=Your-OpenWrt-2.4G/' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
-	
-	# 替换 5G 的 .ssid
-	sed -i '/band="5g"/, /.*ssid=.*/ s/\.ssid=.*/.ssid=Your-OpenWrt-5G/' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
 #fi
 
 #添加第三方软件源
