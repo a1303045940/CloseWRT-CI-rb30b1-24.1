53d1e4cbf409f254e030a2ee337365f5930b19e9
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-10-27 20:59:49 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -40,7 +40,8 @@ sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7
 	
 	date_version=$(date +"%Y年%m月%d日")
 	orig_version=$(cat "package/emortal/default-settings/files/99-default-settings-chinese" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-	VERSION=$(grep "^PRETTY_NAME=" /etc/os-release | cut -d'=' -f2 | tr -d '"')
+	#VERSION=$(grep "^PRETTY_NAME="package/base-files/files/etc/os-release | cut -d'=' -f2 | tr -d '"')
+	VERSION=$(cat "package/base-files/files/etc/os-release" | grep PRETTY_NAME= | awk -F "'" '{print $2}')
 	#sed -i "s/${orig_version}/R${date_version} by vx:Mr___zjz  /g" package/emortal/default-settings/files/99-default-settings-chinese
 	
 	sed -i "s/${orig_version}/ ${VERSION} 编译日期：${date_version}  by 微信:Mr___zjz  /g" package/emortal/default-settings/files/99-default-settings-chinese
